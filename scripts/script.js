4051df38587ee1b1c1feca2970fefaff11d6df50
Tatum Baldwin <tjbaldwin@students.nic.edu>
2023-12-05 15:58:54 -0800

    tried some javascript

--- scripts/script.js
+++ scripts/script.js
@@ -54,3 +54,68 @@ function convertInch () {
     //display the number
     document.getElementById('result').innerText = centimeters + " cm";
 }
+
+
+
+// DARK MODE
+
+// grab light and dark button
+const darkBtn = document.querySelector('.dark-light');
+
+//grab containers
+const homeContainer1 = document.querySelector('.grid-item1');
+const homeContainer2 = document.querySelector('.grid-item2');
+const homeContainer3 = document.querySelector('.grid-item4');
+const homeContainer4 = document.querySelector('.grid-item6');
+const homeContainer5 = document.querySelector('.grid-item7');
+
+const calcContainer1 = document.querySelector('.calc-item1');
+const calcContainer2 = document.querySelector('.calc-item2');
+const calcContainer3 = document.querySelector('.calc-item3');
+
+const contactContainer1 = document.querySelector('.contact1');
+const contactContainer2 = document.querySelector('.contact2');
+const contactContainer3 = document.querySelector('.contact3');
+
+//grab background
+const background = document.querySelector('body');
+
+//grab buttons
+const buttons = document.querySelector('.cta-btn');
+
+//grab text
+const text = document.querySelector('p');
+
+//grab 1 headers
+const mainHeadings = document.querySelector('h1');
+
+//grab 2 headers
+const secondHeadings = document.querySelector('h2');
+
+//create function for light btn
+darkBtn.addEventListener('click', function () {
+    // all toggles
+    background.classList.toggle('dark-mode-background');
+
+    //home page toggles
+    //toggle home containers
+    homeContainer1.classList.toggle('dark-mode-container');
+    homeContainer2.classList.toggle('dark-mode-container');
+    homeContainer3.classList.toggle('dark-mode-container');
+    homeContainer4.classList.toggle('dark-mode-container');
+    homeContainer5.classList.toggle('dark-mode-container');
+
+    //calc page toggles
+    //toggle calc containers
+    calcContainer1.classList.toggle('dark-mode-container');
+    calcContainer2.classList.toggle('dark-mode-container');
+    calcContainer3.classList.toggle('dark-mode-container');
+
+    //contact page toggles
+    //toggle contact containers
+    contactContainer1.classList.toggle('dark-mode-container');
+    contactContainer2.classList.toggle('dark-mode-container');
+    contactContainer3.classList.toggle('dark-mode-container');
+
+
+})
